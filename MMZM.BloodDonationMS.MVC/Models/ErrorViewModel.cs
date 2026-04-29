7cc30b02b0cfecdc1fcc6ff8ab7734a003e070e6
EmiriMaung <poelay1995@gmail.com>
2026-04-29 13:04:17 +0630

    Remove some files

--- MMZM.BloodDonationMS.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MMZM.BloodDonationMS.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
